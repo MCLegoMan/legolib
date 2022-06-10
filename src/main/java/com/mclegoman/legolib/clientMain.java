9607affe0ae77fb78af87b1240a81e9f4ab1e1f7
MCLegoMan <46882277+MCLegoMan@users.noreply.github.com>
2022-06-10 23:24:56 +1200

    Added Language Assets for Keybinds.

--- src/main/java/com/mclegoman/legolib/clientMain.java
+++ src/main/java/com/mclegoman/legolib/clientMain.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class clientMain implements ClientModInitializer {
 
     private static boolean originsInstalled;
-    public List<String> keys = List.of("ternary","quaternary","quinary","senary","septenary","octonary","nonary","denary");
+    public List<String> keys = List.of("one","two","three","four","five","six","seven","eight");
 
     @Override
     public void onInitializeClient() {
@@ -30,7 +30,7 @@ public class clientMain implements ClientModInitializer {
         if(originsInstalled == true) {
             for(String key : keys) {
                 KeyBinding binding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                        "legolib.keybind."+key+"_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "legolib.keybinds"));
+                        "legolib.keybind.origins."+key+"_active", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, "legolib.keybinds.origins"));
                 ApoliClient.registerPowerKeybinding(binding.getTranslationKey(), binding);
                 ApoliClient.registerPowerKeybinding(key, binding);
             }
